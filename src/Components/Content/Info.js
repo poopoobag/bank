13e06459607dd7b8d3e36aa142d94e19c5c7112c
poopoobag <87016044+poopoobag@users.noreply.github.com>
2021-11-19 18:18:54 +0800

    Update Info.js

--- src/Components/Content/Info.js
+++ src/Components/Content/Info.js
@@ -1,7 +1,7 @@
 const Info = props => {
     return <div className='flex justify-between mb-10'>
         <h1>{props.first}</h1>
-        <h1 className='border-b border-green-600'>{props.second}</h1>
+        <h1 className='border-b n border-green-600'>{props.second}</h1>
     </div>
 }
 
